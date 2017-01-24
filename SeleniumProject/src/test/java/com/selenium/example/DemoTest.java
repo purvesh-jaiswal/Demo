1865b613e955feb01ff6de4b3cc7662fc36cfcb4
purvesh-jaiswal <purvesh@appliedcloudcomputing.com>
2017-01-24 17:16:13 +0530

    commit for test this is fist test of demo project

---
+++ SeleniumProject/src/test/java/com/selenium/example/DemoTest.java
@@ -0,0 +1,15 @@
+package com.selenium.example;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void start(){
+		
+		System.out.println("hello selenium ... this is first test on selenium");
+		
+	}
+	
+	
+}
