1c14aa3b83cc4ed4c4597752b36051bbd6eb7c3d
purvesh-jaiswal <purvesh@appliedcloudcomputing.com>
2017-02-20 17:23:23 +0530

    test

--- SeleniumProject/src/test/java/com/selenium/example/DemoTest.java
+++ SeleniumProject/src/test/java/com/selenium/example/DemoTest.java
@@ -13,7 +13,7 @@ private WebDriver driver;
 	
 	@BeforeTest
 	public void setup(){
-		 System.setProperty("webdriver.chrome.driver", "chromedriver");
+		 System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		driver = new ChromeDriver();
 		
 	}
